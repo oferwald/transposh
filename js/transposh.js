4b4e128a15ac22c2b169f466ec32f10d4687800e
Ofer Wald <oferwald+git@transposh.com>
2009-03-23 21:11:51 +0000

    Naturally, a typo slipped in, also fixed the missing logo image in the language list mode (widget)

--- js/transposh.js
+++ js/transposh.js
@@ -169,8 +169,8 @@ function do_auto_translate() {
 //to run at start
 jQuery.noConflict();
 var transposh_post_url,transposh_target_lang; 
-jQuery("script[src*='transposh']").each(function (i) {
-	transposh_post_url = this.src.match('post_url=(.*?)&')[1];
+jQuery("script[src*='transposh.js']").each(function (i) {
+	transposh_post_url = unescape(this.src.match('post_url=(.*?)&')[1]);
 	transposh_target_lang = this.src.match('lang=(.*?)&')[1];
 });
 google.load("language", "1");
