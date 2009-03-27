8807575f695624b3556596fb5da76138c4dc829f
Ofer Wald <oferwald+git@transposh.com>
2009-03-27 18:51:35 +0000

    Changes for creating a "debug" build

--- build.sh
+++ build.sh
@@ -5,16 +5,17 @@
 #
 
 VERSION=$1;
+DEBUG=$2;
+ZIPME=$3;
 
 if [ -z $VERSION ]; then
-    echo "Must enter a version number !!!"
-    echo "Usage: $0 0.1.0"
-    exit
+  echo "Must enter a version number !!!"
+  echo "Usage: $0 0.1.0 [[debug] zip]"
+  exit
 fi
 
 TMP_DIR="tmp"
 TRANSPOSH_DIR=$TMP_DIR/transposh
-
 
 echo "Building package for WordPress plugin version: $VERSION";
  
@@ -29,8 +30,8 @@ echo
 #Add sub directories
 #
 for DIR in flags js; do
-    cp -r $DIR $TRANSPOSH_DIR
-    echo "added sub-directory $DIR"
+  cp -r $DIR $TRANSPOSH_DIR
+  echo "added sub-directory $DIR"
 done;
 echo
 
@@ -38,20 +39,24 @@ echo
 #Add non-php files 
 #
 for FTYPE in css png txt; do
-    cp *.$FTYPE $TRANSPOSH_DIR
-    echo "added $FTYPE files"
+  cp *.$FTYPE $TRANSPOSH_DIR
+  echo "added $FTYPE files"
 done;
 echo
 
 #
 #Add php files while removing logging operations
 #
-echo "Adding .php files (without logging)"
-
-for file in `find . -maxdepth 1 -iname '*.php' -printf "%p "`; do 
-    sed "s/logger.*;//;s/require_once(\"logging.*//;s/<%VERSION%>/$VERSION/;" $file > $TRANSPOSH_DIR/$file
-    echo "added $file"
-done;
+if [ "$DEBUG" != 'debug' ]; then
+  echo "Adding .php files (without logging)"
+  for file in `find . -maxdepth 1 -iname '*.php' -printf "%p "`; do 
+  sed "s/logger.*;//;s/require_once(\"logging.*//;s/<%VERSION%>/$VERSION/;" $file > $TRANSPOSH_DIR/$file
+  echo "added $file"
+  done;
+else
+  echo "Adding .php files (with logging)"
+  cp *.php $TRANSPOSH_DIR
+fi
 echo
 
 #
@@ -63,21 +68,26 @@ echo "fixing version in readme.txt to $VERSION"
 #
 # Remove logging.php
 #
-
-rm $TRANSPOSH_DIR/logging.php
-echo "removed logging.php"
+if [ "$DEBUG" != 'debug' ]; then
+  rm $TRANSPOSH_DIR/logging.php
+  echo "removed logging.php"
+else
+  rm $TRANSPOSH_DIR/screenshot*.png
+  echo "removed screenshots"
+fi
 
 # Remove .svn dirs
-find tmp -name "*.svn*" -exec rm -rf {} \;
+find tmp -name "*.svn*" -exec rm -rf {} 2>/dev/null \;
 echo "removed .svn dirs"
 
 #
 #Generate zip file
 # 
-#cd $TMP_DIR
-#zip -r "transposh.$VERSION.zip" .
-#cd - >/dev/null
-#mv "$TMP_DIR/transposh.$VERSION.zip" . 
-
-#echo
-#echo "transposh.$VERSION.zip is ready"
+if [ "$ZIPME" == 'zip' ]; then
+  cd $TMP_DIR
+  zip -rq "transposh.$VERSION.zip" .
+  cd - >/dev/null
+  mv "$TMP_DIR/transposh.$VERSION.zip" . 
+  echo
+  echo "transposh.$VERSION.zip is ready"
+fi
