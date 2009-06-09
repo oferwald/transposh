271928ab440f47add82c7b2f1c653f47eec8fc8c
Ofer Wald <ofer@transposh.org>
2009-06-09 10:33:12 +0000

    Added logging,  this function really needs a rewrite by itself

--- core/utils.php
+++ core/utils.php
@@ -27,21 +27,21 @@ require_once("logging.php");
 
 /*
  * Update the given url to include language params.
- * param url - the original url to rewrite
+ * param url - the original url to rewrite (expects full urls)
  * param lang - language code
  * param is_edit - is running in edit mode.
  * param use_params_only - use only parameters as modifiers, i.e. not permalinks
  */
 function rewrite_url_lang_param($url, $lang, $is_edit, $use_params_only=FALSE) {
     global $home_url, $home_url_quoted, $enable_permalinks_rewrite;
-
+    logger("old url: $url, lang: $lang, is_edit: $is_edit",5);
     //logger("home url: $home_url",3);
     //logger("home url_quoted: $home_url_quoted",3);
     //logger("enable_permalinks_rewrite: $enable_permalinks_rewrite",3);
-    logger("url: $url",5);
+    logger("url: $url",6);
     $url = html_entity_decode($url, ENT_NOQUOTES);
     $url = str_replace('&#038;', '&', $url);
-    logger("urldec: $url",5);
+    logger("urldec: $url",6);
     
     //remove prev lang and edit params?
     $url = preg_replace("/(" . LANG_PARAM . "|" . EDIT_PARAM . ")=[^&]*/i", "", $url);
@@ -51,9 +51,9 @@ function rewrite_url_lang_param($url, $lang, $is_edit, $use_params_only=FALSE) {
         $use_params_only = TRUE;
     }
 
+    $params ="";
     if($is_edit) {
         $params = EDIT_PARAM . '=1&';
-
     }
 
     if($use_params_only) {
@@ -63,10 +63,12 @@ function rewrite_url_lang_param($url, $lang, $is_edit, $use_params_only=FALSE) {
         $url = preg_replace("/$home_url_quoted\/(..(-..)?\/)?\/?/",
             "$home_url/$lang/",  $url);
     }
+    logger("params: $params",6);
 
     if($params) {
     //insert params to url
         $url = preg_replace("/(.+\/[^\?\#]*[\?]?)/", '$1?' . $params, $url);
+        logger("new url2: $url",6);
 
         //Cleanup extra &
         $url = preg_replace("/&&+/", "&", $url);
@@ -80,7 +82,7 @@ function rewrite_url_lang_param($url, $lang, $is_edit, $use_params_only=FALSE) {
     $url = preg_replace("/\?$/", "", $url);
 
     $url = htmlentities($url, ENT_NOQUOTES);
-
+    logger("new url: $url",5);
     return $url;
 }
 
