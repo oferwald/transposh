60a2606d14f1a8ab319dee9c974e00932c8975d2
Ofer Wald <oferwald+git@transposh.com>
2009-11-20 21:02:03 +0000

    Improved documentation of logger class

--- core/logging.php
+++ core/logging.php
@@ -1,54 +1,67 @@
 <?php
-/*
- * Logging Utils.
+/*  Copyright © 2009 Transposh Team (website : http://transposh.org)
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-//
-//Debug/Logging parameters
-//
+/*
+ * Logging utility class
+ */
 
 // Report all PHP errors
 //error_reporting(E_ALL);
 require_once('FirePHP.class.php');
 
 class logger {
-//Enable tracing level.
-//0 - disabled. Higher numbers will show more debug information.
+    /** @var int Tracing level, 0 is disabled (almost) and higher numbers show more debug info */
     private $debug_level = 3;
-//if (!defined('DEBUG')) define ("DEBUG" , 3);
-
+    /** @var boolean should logging be outputted to stdout */
     private $printout = false;
+    /** @var boolean should logging outputted to stdout include an EOL */
+    private $eolprint = false;
+    /** @var boolean shell we show which function called the logger */
     public $show_caller = true;
+    /** @var FirePHP used for outputing into firephp output */
     private $firephp;
-    /**
-     * Singleton instance of FirePHP
-     *
-     * @var logger
-     */
+    /** @var logger Singelton instance of our logger */
     protected static $instance = null;
-//require_once("parser.php");
 
     function  __construct() {
+        // If not outputting to stdout, we should buffer so firephp will work
         if (!$this->printout) {
             ob_start();
         }
-//$GLOBALS['firephp'] = FirePHP::getInstance(true);
         $this->firephp = FirePHP::getInstance(true);
     }
 
-/*
- * Print a message to log.
- */
+    /**
+     * Print a message to log.
+     * @param mixed $msg
+     * @param int $severity
+     */
     function do_log($msg, $severity=3) {
         if($severity <= $this->debug_level) {
             if ($this->show_caller) {
                 $trace = debug_backtrace();
                 $log_prefix =str_pad("{$trace[2]['class']}::{$trace[2]['function']} {$trace[1]['line']}",55,'_');
             }
-            error_log(date(DATE_RFC822) . ": "  . $msg . "\n", 3,  "/tmp/transposh.log");
+            if (!is_object($msg))
+                error_log(date(DATE_RFC822) . ": "  . $msg . "\n", 3,  "/tmp/transposh.log");
             if ($this->printout || !isset($this->firephp)) {
                 echo $msg;
-                echo (defined('EOLPRINT')) ? "\n" : "<br/>";
+                echo ($this->eolprint) ? "\n" : "<br/>";
             }
             else {
                 if ((is_array($msg) || is_object($msg)) && $this->show_caller) {
@@ -68,10 +81,9 @@ class logger {
     }
 
     /**
-     * Gets singleton instance of FirePHP
-     *
+     * Gets singleton instance of logger
      * @param boolean $AutoCreate
-     * @return FirePHP
+     * @return logger
      */
     public static function getInstance($AutoCreate=false) {
         if($AutoCreate===true && !self::$instance) {
@@ -81,23 +93,31 @@ class logger {
     }
 
     /**
-     * Creates FirePHP object and stores it for singleton access
-     *
-     * @return FirePHP
+     * Creates logger object and stores it for singleton access
+     * @return logger
      */
     public static function init() {
         return self::$instance = new self();
     }
 }
 
+// We create a global singelton instance
 $GLOBALS['logger'] = logger::getInstance(true);
 
+/**
+ * This function provides easier access to logging using the singleton object
+ * @param mixed $msg
+ * @param int $severity
+ */
 function logger($msg, $severity=3) {
     $GLOBALS['logger']->do_log($msg,$severity);
 }
 
-// sample
-//$GLOBALS['logger'] = logger::getInstance(true);
-//$GLOBALS['logger']->show_caller = true;
+/*
+*  sample of how to modify logging parameters from anywhere
+* 
+$GLOBALS['logger'] = logger::getInstance(true);
+$GLOBALS['logger']->show_caller = true;
+ */
 
 ?>
