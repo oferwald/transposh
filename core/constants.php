2ce12fbad8908ebcee5ad0bb1280c641a89d0d75
Ofer Wald <ofer@transposh.org>
2010-03-01 22:38:41 +0000

    Microsoft translate added 7 more languages - new total of 30

--- core/constants.php
+++ core/constants.php
@@ -92,7 +92,7 @@ $rtl_languages = array("ar", "he", "fa", "yi");
 //Google supported languages @updated 2009-Dec-21
 $google_languages = array("en", "af", "sq", "ar", "be", "bg", "ca", "zh", "zh-tw", "hr", "cs", "da", "nl", "et", "fi", "fr", "gl", "de", "el", "ht", "he", "hi", "hu", "id", "it", "is", "ga", "ja", "ko", "lv", "lt", "mk", "ms", "mt", "no", "fa", "pl", "pt", "ro", "ru", "sr", "sk", "sl", "es", "sw", "sv", "tl", "th", "tr", "uk", "vi", "cy", "yi");
 //Bing supported languages @updated 2009-Dec-21
-$bing_languages = array("en", "ar", "bg", "zh", "zh-tw", "cs", "da", "nl", "ht", "fi", "fr", "de", "gr", "he", "it", "ja", "ko", "pl", "pt", "ru", "es", "sv", "th");
+$bing_languages = array("en", "ar", "bg", "zh", "zh-tw", "cs", "da", "nl", "ht", "fi", "fr", "de", "gr", "he", "hu", "it", "ja", "ko", "lt", "no", "pl", "pt", "ro", "ru", "sk", "sl", "es", "sv", "th", "tr");
 
 //Define the new capability that will be assigned to roles - translator
 define("TRANSLATOR", 'translator');
