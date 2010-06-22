bf2afd8c521e3f334a295843c1e15da6325b9156
Ofer Wald <oferwald+git@transposh.com>
2010-06-22 08:13:10 +0000

    Avoid translation of trashed and draft posts on translate all button

--- wp/transposh_ajax.php
+++ wp/transposh_ajax.php
@@ -110,7 +110,7 @@ elseif (isset($_GET['tr_cookie_bck'])) {
 // Start full translation
 elseif (isset($_GET['translate_all'])) {
     // get all ids in need of translation
-    $page_ids = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type='page' OR post_type='post' ORDER BY ID DESC");
+    $page_ids = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE (post_type='page' OR post_type='post') AND (post_status='publish' OR post_status='private') ORDER BY ID DESC");
     // only high capabilities users can...
     if (!current_user_can('edit_post',$page_ids[0])) return;
     echo json_encode($page_ids);
