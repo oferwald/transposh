272d1185f845862adc44993c61bd3d543fdb9e0c
Ofer Wald <oferwald+git@transposh.com>
2010-07-30 18:31:48 +0000

    Allow some object logging to the log file

--- core/logging.php
+++ core/logging.php
@@ -25,6 +25,8 @@
 //error_reporting(E_ALL);
 require_once('FirePHP.class.php');
 
+define('TP_LOG_FILE', '/tmp/transposh.log');
+
 class logger {
 
     /** @var int Tracing level, 0 is disabled (almost) and higher numbers show more debug info */
@@ -41,11 +43,11 @@ class logger {
     protected static $instance = null;
 
     function __construct() {
-	// If not outputting to stdout, we should buffer so firephp will work
-	if (!$this->printout) {
-	    ob_start();
-	}
-	$this->firephp = FirePHP::getInstance(true);
+        // If not outputting to stdout, we should buffer so firephp will work
+        if (!$this->printout) {
+            ob_start();
+        }
+        $this->firephp = FirePHP::getInstance(true);
     }
 
     /**
@@ -54,48 +56,51 @@ class logger {
      * @param int $severity
      */
     function do_log($msg, $severity=3) {
-	if ($severity <= $this->debug_level) {
-	    if ($this->show_caller) {
-		$trace = debug_backtrace();
-		if ($trace[2]['class']) {
-		    $log_prefix = str_pad("{$trace[2]['class']}::{$trace[2]['function']} {$trace[1]['line']}", 55, '_');
-		} else {
-		    $prefile = substr($trace[1]['file'], strrpos($trace[1]['file'], "/"));
-		    $log_prefix = str_pad("{$prefile}::{$trace[1]['function']} {$trace[1]['line']}", 55, '_');
-		}
-	    }
-	    if (!is_object($msg)) {
-		if (!is_array($msg)) {
-		    error_log(date(DATE_RFC822) . " $log_prefix: " . $msg . "\n", 3, "/tmp/transposh.log");
-		} else {
-		    error_log(date(DATE_RFC822) . " $log_prefix: Array start\n", 3, "/tmp/transposh.log");
-		    foreach ($msg as $key => $item) {
-			if (!is_array($item)) {
-			    error_log(date(DATE_RFC822) . " $log_prefix: $key => $item\n", 3, "/tmp/transposh.log");
-			} else {
-			    error_log(date(DATE_RFC822) . " $log_prefix: subarray -> $key\n", 3, "/tmp/transposh.log");
-			    $this->do_log($item, $severity);
-			}
-		    }
-		    error_log(date(DATE_RFC822) . " $log_prefix: Array stop\n", 3, "/tmp/transposh.log");
-		}
-	    }
-	    if ($this->printout || !isset($this->firephp)) {
-		echo "$log_prefix:$msg";
-		echo ($this->eolprint) ? "\n" : "<br/>";
-	    } else {
-		if ((is_array($msg) || is_object($msg)) && $this->show_caller) {
-		    $this->firephp->log("$log_prefix:");
-		    $this->firephp->log($msg);
-		} else {
-		    if (is_array($msg) || is_object($msg)) {
-			$this->firephp->log($msg);
-		    } else {
-			$this->firephp->log("$log_prefix:$msg");
-		    }
-		}
-	    }
-	}
+        if ($severity <= $this->debug_level) {
+            if ($this->show_caller) {
+                $trace = debug_backtrace();
+                if ($trace[2]['class']) {
+                    $log_prefix = str_pad("{$trace[2]['class']}::{$trace[2]['function']} {$trace[1]['line']}", 55, '_');
+                } else {
+                    $prefile = substr($trace[1]['file'], strrpos($trace[1]['file'], "/"));
+                    $log_prefix = str_pad("{$prefile}::{$trace[1]['function']} {$trace[1]['line']}", 55, '_');
+                }
+            }
+            if (!is_array($msg) && !is_object($msg)) {
+                error_log(date(DATE_RFC822) . " $log_prefix: " . $msg . "\n", 3, TP_LOG_FILE);
+            } else {
+                if (is_array($msg)) {
+                    error_log(date(DATE_RFC822) . " $log_prefix: Array start\n", 3, TP_LOG_FILE);
+                } else {
+                    error_log(date(DATE_RFC822) . " $log_prefix: Object start\n", 3, TP_LOG_FILE);
+                }
+                foreach ($msg as $key => $item) {
+                    if (!is_array($item)) {
+                        if (!is_object($item) || method_exists($item, '__toString'))
+                                error_log(date(DATE_RFC822) . " $log_prefix: $key => $item\n", 3, TP_LOG_FILE);
+                    } else {
+                        error_log(date(DATE_RFC822) . " $log_prefix: subarray -> $key\n", 3, TP_LOG_FILE);
+                        $this->do_log($item, $severity);
+                    }
+                }
+                error_log(date(DATE_RFC822) . " $log_prefix: Array stop\n", 3, TP_LOG_FILE);
+            }
+            if ($this->printout || !isset($this->firephp)) {
+                echo "$log_prefix:$msg";
+                echo ($this->eolprint) ? "\n" : "<br/>";
+            } else {
+                if ((is_array($msg) || is_object($msg)) && $this->show_caller) {
+                    $this->firephp->log("$log_prefix:");
+                    $this->firephp->log($msg);
+                } else {
+                    if (is_array($msg) || is_object($msg)) {
+                        $this->firephp->log($msg);
+                    } else {
+                        $this->firephp->log("$log_prefix:$msg");
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -104,10 +109,10 @@ class logger {
      * @return logger
      */
     public static function getInstance($AutoCreate=false) {
-	if ($AutoCreate === true && !self::$instance) {
-	    self::init();
-	}
-	return self::$instance;
+        if ($AutoCreate === true && !self::$instance) {
+            self::init();
+        }
+        return self::$instance;
     }
 
     /**
@@ -115,11 +120,11 @@ class logger {
      * @return logger
      */
     public static function init() {
-	return self::$instance = new self();
+        return self::$instance = new self();
     }
 
     public function set_debug_level($int) {
-	$this->debug_level = $int;
+        $this->debug_level = $int;
     }
 
 }
