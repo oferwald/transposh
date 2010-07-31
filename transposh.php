850a122c90c224057855e50c8b556c0f9cdf67b1
Ofer Wald <ofer@transposh.org>
2010-07-31 23:32:06 +0000

    Small change in buddypress activity stream

--- transposh.php
+++ transposh.php
@@ -805,8 +805,8 @@ class transposh_plugin {
         // just got this from buddypress, changed action and content
 	$values = array(
 		'user_id' => $bp->loggedin_user->id,
-		'action' => sprintf(__('%s translated a pharse with transposh:', 'buddypress'), bp_core_get_userlink($bp->loggedin_user->id)),
-		'content' => "Original:<span class=\"no_translate\">$original</span>\nTranslation:<span class=\"no_translate\">$translation</span>",
+		'action' => sprintf(__('%s translated a pharse to %s with transposh:', 'buddypress'), bp_core_get_userlink($bp->loggedin_user->id),substr($GLOBALS['languages'][$lang],0,strpos($GLOBALS['languages'][$lang],','))),
+		'content' => "Original: <span class=\"no_translate\">$original</span>\nTranslation: <span class=\"no_translate\">$translation</span>",
 		'primary_link' => '',
 		'component' => $bp->blogs->id,
 		'type' => 'new_translation',
