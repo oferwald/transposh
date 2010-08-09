7feadab3160e17414c2df2e874ec07893a00fdfc
Ofer Wald <ofer@transposh.org>
2010-08-09 10:40:36 +0000

    Use a shorter, but slightly more informative copyright notice

--- widgets/flagslist/tpw_list_with_flags.php
+++ widgets/flagslist/tpw_list_with_flags.php
@@ -10,21 +10,15 @@
   License: GPL (http://www.gnu.org/licenses/gpl.txt)
  */
 
-/*  Copyright © 2009-2010 Transposh Team (website : http://transposh.org)
+/*
+ * Transposh v%VERSION%
+ * http://transposh.org/
  *
- * 	This program is free software; you can redistribute it and/or modify
- * 	it under the terms of the GNU General Public License as published by
- * 	the Free Software Foundation; either version 2 of the License, or
- * 	(at your option) any later version.
+ * Copyright %YEAR%, Team Transposh
+ * Licensed under the GPL Version 2 or higher.
+ * http://transposh.org/license
  *
- * 	This program is distributed in the hope that it will be useful,
- * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
- * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * 	GNU General Public License for more details.
- *
- * 	You should have received a copy of the GNU General Public License
- * 	along with this program; if not, write to the Free Software
- * 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Date: %DATE%
  */
 
 /**
@@ -56,9 +50,9 @@ function tp_widget_do($args) {
 
     echo "<div class=\"" . NO_TRANSLATE_CLASS . " transposh_flags\" >";
     foreach ($args as $langrecord) {
-	echo "<a href=\"{$langrecord['url']}\"" . ($langrecord['active'] ? ' class="tr_active"' : '' ) . '>' .
-	transposh_utils::display_flag("$plugpath/img/flags", $langrecord['flag'], $langrecord['langorig'], false) . "</a>";
-	echo "<a href=\"{$langrecord['url']}\"" . ($langrecord['active'] ? ' class="tr_active"' : '' ) . '>' . "{$langrecord['langorig']}</a><br/>";
+        echo "<a href=\"{$langrecord['url']}\"" . ($langrecord['active'] ? ' class="tr_active"' : '' ) . '>' .
+        transposh_utils::display_flag("$plugpath/img/flags", $langrecord['flag'], $langrecord['langorig'], false) . "</a>";
+        echo "<a href=\"{$langrecord['url']}\"" . ($langrecord['active'] ? ' class="tr_active"' : '' ) . '>' . "{$langrecord['langorig']}</a><br/>";
     }
     echo "</div>";
 }
