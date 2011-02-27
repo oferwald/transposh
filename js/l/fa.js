c86624f389bb247923dc66ddff22347a2dc1a281
Ofer Wald <oferwald+git@transposh.com>
2011-02-27 21:18:06 +0000

    Persian support by Sushyant Zavarzadeh , Italian by Mac Italy, Spanish by Angel Torregrosa

---
+++ js/l/fa.js
@@ -0,0 +1,39 @@
+/*
+ * Transposh v0.7.1
+ * http://transposh.org/
+ *
+ * Copyright 2011,    Team Transposh
+ * Licensed under the GPL Version 2 or higher.
+ * http://transposh.org/license
+ *
+ * Date:  Sun,    30 Jan 2011 12: 58: 12 +0200
+ */
+t_jp.l={
+    'Close without saving?': 'بدون نگه داری ببندیم؟',
+    'You have made a change to the translation. Are you sure you want to discard it?': 'شما در ترجمه یک دگرگونی ساخته اید آیا آگاهانه می خواهید آن را دور بیندازید؟ ',
+    'History': 'پیشینه',
+    'Loading...': 'بارگیری...',
+    'Translated': 'ترجمه شده',
+    'By': 'با',
+    'At': 'در',
+    'google': 'گوگل',
+    'bing': 'بینگ',
+    'apertium': 'اِی پِرشِم',
+    'manual translation': 'ترجمه ی دستی',
+    'bing suggest': 'پیشنهاد بینگ',
+    'google suggest': 'پیشنهاد گوگل',
+    'apertium suggest': 'پیشنهاد اِی پِرشِم',
+    'Edit Translation': 'ویرایش ترجمه',
+    'Original text': 'نوشته ی پایه',
+    'read alternate translations': 'خواندن ترجمه های جایگزین',
+    'previous translation': 'ترجمه ی پیشین',
+    'find on page': 'یافتن در برگه',
+    'next translation': 'ترجمه ی (بعدی) پسین',
+    'Translate to': 'ترجمه به',
+    'view translation log': 'دیدن رویدادهای ترجمه',
+    'virtual keyboard': 'کیبورد مجازی',
+    'approve translation': 'پذیرش ترجمه',
+    'delete': 'پاک کردن',
+    'Discard': 'دور انداختن',
+    'Cancel': 'پرهیز'
+};
