a583f2e9b90f7c940ce2cdfab32c5ca578e8d79b
Ofer Wald <oferwald+git@transposh.com>
2011-05-30 21:02:00 +0000

    German support by Jenny Beelens. http://www.professionaltranslation.com.

---
+++ js/l/de.js
@@ -0,0 +1,32 @@
+/*
+ * Thanks: Jenny Beelens (http://www.professionaltranslation.com)
+ */
+t_jp.l = {
+    'Close without saving?': 'Schließen ohne Speichern?',
+    'You have made a change to the translation. Are you sure you want to discard it?': 'Sie haben die Übersetzung bearbeitet. Sind Sie sicher, dass Sie abbrechen wollen?',
+    'History': 'Verlauf',
+    'Loading...': 'Laden...',
+    'Translated': 'Übersetzt',
+    'By': 'von',
+    'At': 'über',
+    'google': 'Google',
+    'bing': 'Bing',
+    'apertium': 'Apertium',
+    'manual translation': 'manuelle Übersetzung',
+    'bing suggest': 'Bing Suggest',
+    'google suggest': 'Google Suggest',
+    'apertium suggest': 'Apertium Suggest',
+    'Edit Translation': 'Übersetzung bearbeiten',
+    'Original text': 'Originaltext',
+    'read alternate translations': 'alternative Übersetzung lesen',
+    'previous translation': 'Vorherige',
+    'find on page': 'Auf Seite suchen',
+    'next translation': 'Nächste',
+    'Translate to': 'Übersetzen nach',
+    'view translation log': 'Übersetzungsprotokoll anzeigen',
+    'virtual keyboard': 'virtuelle Tastatur',
+    'approve translation': 'Übersetzung genehmigen',
+    'delete': 'Löschen',
+    'Discard': 'Verwerfen',
+    'Cancel': 'Abbrechen'
+};
