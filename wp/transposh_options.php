be9b31e46f311b5b14ce036720ff6cc8fa449803
Ofer Wald <ofer@transposh.org>
2011-07-31 01:06:43 +0000

    Display warnings for some conditions we can detect (low memory, no cache support)

--- wp/transposh_options.php
+++ wp/transposh_options.php
@@ -72,6 +72,8 @@ define('WIDGET_THEME', 'widget_theme');
 define('TRANSPOSH_KEY', 'transposh_key');
 //Stores the site key to transposh services (backup @since 0.5.0)
 define('TRANSPOSH_BACKUP_SCHEDULE', 'transposh_backup_schedule');
+//Stores hidden warnings (@since 0.7.6)
+define('TRANSPOSH_ADMIN_HIDE_WARNINGS', 'transposh_admin_hide_warnings');
 
 class transposh_plugin_options {
 //constructor of class, PHP4 compatible construction for backward compatibility
@@ -113,6 +115,7 @@ class transposh_plugin_options {
         $this->set_default_option_value(WIDGET_THEME, 'ui-lightness');
         $this->set_default_option_value(TRANSPOSH_KEY);
         $this->set_default_option_value(TRANSPOSH_BACKUP_SCHEDULE);
+        $this->set_default_option_value(TRANSPOSH_ADMIN_HIDE_WARNINGS);
         $this->migrate_old_config();
         logger($this->options, 4);
     }
@@ -266,6 +269,10 @@ class transposh_plugin_options {
         return $this->options[TRANSPOSH_DEFAULT_LOCALE_OVERRIDE];
     }
 
+    function get_transposh_admin_hide_warning($id) {
+        return strpos($this->options[TRANSPOSH_ADMIN_HIDE_WARNINGS], $id . ',') !== false;
+    }
+
     /**
      * Sets a value at the options array
      * @param mixed $val
@@ -314,7 +321,7 @@ class transposh_plugin_options {
         $this->set_value($val, $this->options[WIDGET_FILE]);
     }
 
-     /**
+    /**
      * Set the widget theme
      * @since 0.7.0
      * @param string $val
@@ -400,6 +407,12 @@ class transposh_plugin_options {
     function set_transposh_default_locale_override($val) {
         $val = ($val) ? 1 : 0;
         $this->set_value($val, $this->options[TRANSPOSH_DEFAULT_LOCALE_OVERRIDE]);
+    }
+
+    function set_transposh_admin_hide_warning($id) {
+        if (!$this->get_transposh_admin_hide_warning($id)) {
+            $this->set_value($this->options[TRANSPOSH_ADMIN_HIDE_WARNINGS] . $id . ',', $this->options[TRANSPOSH_ADMIN_HIDE_WARNINGS]);
+        }
     }
 
     /**
