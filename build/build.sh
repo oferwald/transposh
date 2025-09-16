4b4fec5e81d564cab3ca87aa1f7fabe3e6573d1f
Ofer Wald <oferwald+git@transposh.com>
2025-09-17 02:19:29 +0300

    Some fixes to the build script

--- build/build.sh
+++ build/build.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 #
-#Script for packaging Transposh's plugin for WordPress
+# Script for packaging Transposh's plugin for WordPress
 #
 
 VERSION=$1
 ZIPME=$2
 COPYTO=$3
-WPO=$4
 MINIFY=true
-
-if [ -n "$WPO" ]; then
-  echo "Version supplied $WPO"
-else
-  WPO="full"
-fi
 
 #
 # Init replacement vars
@@ -22,13 +15,7 @@ fi
 DATE=`date -R`
 YEAR=`date +%Y`
 
-if [ $WPO == 'wporg' ]
-then
-  ZIPME=false
-  DIRS="js css img langs wp"
-else
-  DIRS="js css img langs widgets wp"
-fi
+DIRS="js css img langs widgets wp"
 
 if [ -z $VERSION ]; then
   echo "Must enter a version number !!!"
@@ -39,15 +26,15 @@ fi
 TRANSPOSH_DIR=/tmp/transposh-translation-filter-for-wordpress
 
 echo "Building package for WordPress plugin version: $VERSION";
- 
-#Cleanup tmp dir 
+
+# Cleanup tmp dir
 rm -r $TRANSPOSH_DIR 2>/dev/null
 mkdir $TRANSPOSH_DIR
 echo "cleaned up $TRANSPOSH_DIR directory"
 echo
 
 #
-#Add sub directories
+# Add sub directories
 #
 for DIR in $DIRS; do
   cp -r $DIR $TRANSPOSH_DIR
@@ -55,15 +42,8 @@ for DIR in $DIRS; do
 done
 echo
 
-if [ $WPO == 'wporg' ]
-then
-  echo "only add default widget"
-  mkdir -p $TRANSPOSH_DIR/widgets/default
-  cp -r widgets/default $TRANSPOSH_DIR/widgets/default
-fi
-
 #
-#Create core directory
+# Create core directory
 #
 
 mkdir $TRANSPOSH_DIR/core
@@ -71,7 +51,7 @@ mkdir $TRANSPOSH_DIR/core/shd
 mkdir $TRANSPOSH_DIR/core/jsonwrapper
 
 #
-#Add non-php files
+# Add non-php files
 #
 
 for FTYPE in txt; do
@@ -84,46 +64,34 @@ echo
 # Add php files while processing versions
 #
 
-if [ $WPO == 'wporg' ]
-then
-  PHPFILES=`find . -maxdepth 4 -iname '*.php' -not -path "./build/*" -not -path "./resources/*" -not -path "./test/*" -not -path "./widgets/*"`
-  PHPFILES+=" ./widgets/default/tpw_default.php"
-else
-  PHPFILES=`find . -maxdepth 4 -iname '*.php' -not -path "./build/*" -not -path "./resources/*" -not -path "./test/*"`
-fi
+PHPFILES=`find . -maxdepth 4 -iname '*.php' -not -path "./build/*" -not -path "./scripts/*" -not -path "./resources/*" -not -path "./test/*"`
 
-  echo "Adding .php files"
-  for file in $PHPFILES; do
-    sed "s/%VERSION%/$VERSION/;s/%DATE%/$DATE/;s/%YEAR%/$YEAR/;" $file > $TRANSPOSH_DIR/$file
-    if [ $WPO == 'wporg' ]
-    then
-      php build/generateversions.php $TRANSPOSH_DIR/$file wporg > /tmp/inp_nope
-    else
-      php build/generateversions.php $TRANSPOSH_DIR/$file full > /tmp/inp_nope
-    fi
-    cp /tmp/inp_nope $TRANSPOSH_DIR/$file
-    rm /tmp/inp_nope
-    echo "added $file"
-  done;
+echo "Adding .php files"
+for file in $PHPFILES; do
+  sed "s/%VERSION%/$VERSION/;s/%DATE%/$DATE/;s/%YEAR%/$YEAR/;" $file > $TRANSPOSH_DIR/$file
+  #php build/generateversions.php $TRANSPOSH_DIR/$file full > /tmp/inp_nope
+  #cp /tmp/inp_nope $TRANSPOSH_DIR/$file
+  #rm /tmp/inp_nope
+  echo "added $file"
+done;
 echo
 
 #
-#Add the index.html
+# Add the index.html
 #
 
 echo "Adding index.html"
 cp index.html $TRANSPOSH_DIR/index.html
 
 #
-#fixing version in readme.txt
+# Fixing version in readme.txt
 #
 
 echo "fixing version in readme.txt to $VERSION"
 sed "s/%VERSION%/$VERSION/;" readme.txt > $TRANSPOSH_DIR/readme.txt
 
-# those are the files that are lazy loaded, we make sure they will be read by the browser with the sourceURL comment
+# Those are the files that are lazy loaded, we make sure they will be read by the browser with the sourceURL comment
 LAZYLIST="transposhedit.js de.js es.js fa.js fr.js he.js it.js nl.js ru.js tr.js keyboard.js lazy.js jquery.ui.menu.js"
-#if [ "$DEBUG" != 'debug' ]; then
 if [ $MINIFY == true ]; then
   echo "Minify .js files"
   for file in `find ./js -maxdepth 3 -iname '*.js'`; do
@@ -139,14 +107,14 @@ if [ $MINIFY == true ]; then
   done
 
   echo "Minify .css files"
-  for file in `find . -maxdepth 2 -iname '*.css'`; do 
+  for file in `find . -maxdepth 2 -iname '*.css' -not -name 'circle-flags.css' -not -name 'flags.css' -not -name 'square-flags.css'`; do
     echo "minifying $file"
     java -jar /root/yui/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar $file -o $TRANSPOSH_DIR/$file
   done;
 fi
 
 #
-#Generate zip file
+# Generate zip file
 #
 
 if [ "$ZIPME" == 'zip' ]; then
@@ -161,7 +129,7 @@ if [ "$ZIPME" == 'zip' ]; then
 fi
 
 #
-#Copy to target
+# Copy to target
 #
 
 if [ "$COPYTO" != '' ]; then
